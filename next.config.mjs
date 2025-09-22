ea110a75c4837dfbc216c2ff6cefde8c80893e55
ntuananhdevs <anhndt25@gmail.com>
2025-09-22 10:26:01 +0700

    Add sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
